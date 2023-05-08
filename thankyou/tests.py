66a589b81db0b7303417d68b64fd503085b09e0d
Mario Hernandez <mariofix@proton.me>
2023-05-08 06:23:29 -0400

    Tests

--- thankyou/tests.py
+++ thankyou/tests.py
@@ -1,6 +1,11 @@
 from .thankyou import give_thanks
 
 
-def test_thanks():
+def test_thanks_string():
     thanks_string = give_thanks()
     assert isinstance(thanks_string, str)
+
+
+def test_thanks_tuple():
+    thanks_string = give_thanks(as_tuple=True)
+    assert isinstance(thanks_string, tuple)
